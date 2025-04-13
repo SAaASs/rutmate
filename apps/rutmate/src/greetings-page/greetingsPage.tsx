f9c5a421beb02153b611305f161787b0b875e532
DESKTOP-H8FE04 <andrewcraft789@gmail.com>
2025-04-13 19:57:23 +0300

    greetPage updated

--- apps/rutmate/src/greetings-page/greetingsPage.tsx
+++ apps/rutmate/src/greetings-page/greetingsPage.tsx
@@ -3,7 +3,9 @@ import Typo from '../typo/typo';
 import Button from '../button/button';
 import Substrate from '../substrate/substrate';
 import VerticalContainer from '../vertical-container/verticalContainer';
+import { useNavigate } from 'react-router';
 export function GreetingsPage() {
+  const navigate = useNavigate();
   return (
     <Substrate>
       <VerticalContainer>
@@ -12,9 +14,9 @@ export function GreetingsPage() {
         <Typo marginTop={80} marginBottom={25} color={'#000000'} size={24} weight={600}>Найди соседа-найди друга!</Typo>
         <Typo marginBottom={25} color={'#333333'} size={15} weight={400}>Присоединяйтесь к нам, чтобы найти
           идеального соседа.</Typo>
-        <Button marginBottom={25}><Typo color={'#ffffff'} weight={600} size={18}>Войти</Typo></Button>
+        <Button onClick={()=>{navigate('/sign-in')}} marginBottom={25}><Typo color={'#ffffff'} weight={600} size={18}>Войти</Typo></Button>
         <Typo marginBottom={25} color={'#000000'} size={14} weight={400}>Нет аккаунта?
-          Зарегистрироваться</Typo>
+          <span className={s.link} onClick={()=>{navigate('/sign-up')}}>Зарегистрироваться</span></Typo>
       </VerticalContainer>
     </Substrate>
   );
